2b1ae138efe8fbb7d7b0f9da64f389507c67ec28
Matias <matiasezequielsmania@gmail.com>
2025-03-11 12:32:32 -0300

    .

--- Back/index.js
+++ Back/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');  // Requiere el paquete CORS
 const mqtt = require('mqtt');  // Asegúrate de agregar esta línea
+const { publishMessage } = require("./utils/mqttclients");
 // Crear una instancia de la aplicación
 const app = express();
 
@@ -14,29 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
-// Configuración del cliente MQTT
-const mqttUrl = 'http://175.10.0.127:1080';  // Asegúrate de usar la URL del broker MQTT (localhost si está en el mismo servidor)
-const mqttOptions = {
-  clientId: 'nodejs-client',  // Un identificador único para el cliente MQTT
-  clean: true,  // Si la conexión debe limpiarse al desconectar
-  connectTimeout: 4000,  // Tiempo de espera para conectar
-  reconnectPeriod: 1000,  // Reintentar la conexión cada 1 segundo si se pierde
-};
-
-
-const mqttClient = mqtt.connect(mqttUrl, mqttOptions);
-
-
-// Conectar al broker MQTT
-mqttClient.on('connect', () => {
-    console.log('Conectado al broker MQTT');
-  });
-  
-  // En caso de que haya un error de conexión con el broker MQTT
-  mqttClient.on('error', (err) => {
-    console.error('Error de conexión MQTT:', err);
-  });
-
 
 // Ruta de ejemplo para verificar si el servidor está funcionando
 app.get('/', (req, res) => {
@@ -45,23 +23,13 @@ app.get('/', (req, res) => {
 
 // Una ruta más compleja (por ejemplo, obtener todos los usuarios)
 app.post('/formulario', (req, res) => {
-    // Aquí deberías poder acceder a los datos enviados en la solicitud
-    const nuevoUsuario = req.body;
-    console.log(nuevoUsuario)
-  
- mqttClient.publish('nodered/usuarios', JSON.stringify(nuevoUsuario), (err) => {
-    if (err) {
-      console.error('Error al publicar en el tema MQTT:', err);
-      return res.status(500).json({ mensaje: 'Error al publicar en MQTT' });
-    }
+  // Aquí deberías poder acceder a los datos enviados en la solicitud
+  const nuevoUsuario = req.body;
+  console.log(nuevoUsuario)
 
-    // Responder con los datos recibidos y publicados
-    res.json({
-      mensaje: 'Usuario recibido y publicado en MQTT',
-      usuario: nuevoUsuario,
-    });
-  });
-  });
+  publishMessage(`identificacion_TDB/informacion`, JSON.stringify(nuevoUsuario));
+
+});
 
 // Iniciar el servidor
 app.listen(port, () => {
